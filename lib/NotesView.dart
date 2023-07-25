78ff162131e3f3656ff2a6a527e79de35d13df5a
Your Name <you@example.com>
2023-07-25 12:10:33 +0200

    create folder widget and files

--- lib/NotesView.dart
+++ lib/NotesView.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:tharwat2/Widget/widget.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+
+      body: NotesBody(),
+    );
   }
 }
+
+
+
+
+
+
+
