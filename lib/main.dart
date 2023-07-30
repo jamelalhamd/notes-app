9dedff49d62335d377a3a0ebfaa775d47223bb74
Jamel <J_hamad83@hotmail.com>
2023-07-31 00:22:50 +0200

    privide addnot cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:tharwat2/NotesView.dart';
 import 'package:tharwat2/Widget/NotesBody.dart';
 import 'package:tharwat2/Widget/home.dart';
 import 'package:tharwat2/Widget/models/models.dart';
 import 'package:tharwat2/Widget/resublewidget.dart';
+import 'package:tharwat2/cubit/addnot/addnote_cubit.dart';
 
 Future<void> main() async {
-
   await Hive.initFlutter();
-  
+
   await Hive.openBox(KNoteBox);
-  Hive.registerAdapter(NoteModelAdapter());// the model generated//const KNoteBox='Note Box'; in const page
+  Hive.registerAdapter(
+      NoteModelAdapter()); // the model generated//const KNoteBox='Note Box'; in const page
   runApp(const MyApp());
 }
 
@@ -21,28 +24,35 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => AddnoteCubit(),
 
-      theme: ThemeData(
+        ),
+
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+
+        theme: ThemeData(
 
 
-brightness: Brightness.dark,
-        fontFamily: 'ssets/fonts/Poppins-Regular.ttf'
+            brightness: Brightness.dark,
+            fontFamily: 'ssets/fonts/Poppins-Regular.ttf'
+
+        ),
+        home:
+
+        Scaffold(
+
+          body: NoteHome(),
+
+
+        ),
+
 
       ),
-      home:
-
-      Scaffold(
-
-        body: NoteHome(),
-
-
-      ),
-
-
-
     );
   }
 }
