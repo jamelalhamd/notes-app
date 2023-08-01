94842b4c3f1e5fce3571bf23f9435b8da8ec550f
Jamel <J_hamad83@hotmail.com>
2023-08-01 20:47:38 +0200

    creat viewnote cubit

--- lib/cubit/viewnote/notcubit_cubit.dart
+++ lib/cubit/viewnote/notcubit_cubit.dart
@@ -1,9 +1,48 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:meta/meta.dart';
 import 'package:tharwat2/Widget/models/models.dart';
+import 'package:tharwat2/Widget/resublewidget.dart';
 
 part 'notcubit_state.dart';
 
 class NotcubitCubit extends Cubit<NotcubitState> {
   NotcubitCubit() : super(NotcubitInitial());
+
+
+  fetchAllnotes ()  {
+
+
+
+
+
+
+      try {
+
+
+        var notebox=  Hive.box<NoteModel>(kNoteBox);
+
+         List<NoteModel> notes= notebox.values.toList();
+        emit(Notcubitsuceess(notes));
+
+
+      } catch (e) {
+
+        emit( Notcubitfailure('the error is :   ${e.toString()}'));
+        for(int i=0; i<50;i++)
+        {
+
+          print('the error is :   ${e.toString()}');
+        }
+
+
+
+
+
+
+      }
+
+    }
+
+
 }
